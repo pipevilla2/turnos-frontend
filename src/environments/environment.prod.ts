59e6a83ddb7f8f8abf5b8e35220853d1c5a17652
pipevilla2 <pipevilla2@hotmail.com>
2026-09-14 10:57:54 -0500

    Ajustes en el frontend para conectarse al servicio del backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://turnos-backend-hpgygthvgpcdcbbf.westus3-01.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7150/api'
+  apiUrl: 'https://turnos-backend-hpgygthvgpcdcbbf.westus3-01.azurewebsites.net/api'
 };
